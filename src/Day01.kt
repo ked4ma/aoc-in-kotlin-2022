4a53f68cd84b4327ab035cbd4a91ee7632794c2d
ked4ma <hades.netherworld@gmail.com>
2022-12-02 00:01:38 +0900

    day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,37 @@
+/**
+ * [Day01](https://adventofcode.com/2022/day/1)
+ */
 fun main() {
+    fun convInput(input: List<String>): List<Int> = buildList {
+        var list = ArrayList<Int>()
+        for (s in input) {
+            if (s.isEmpty()) {
+                if (list.size > 0) {
+                    add(list)
+                    list = ArrayList()
+                }
+            } else {
+                list.add(s.toInt())
+            }
+        }
+        if (list.isNotEmpty()) {
+            add(list)
+        }
+    }.map(ArrayList<Int>::sum)
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val conv = convInput(input)
+        return conv.max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val conv = convInput(input)
+        return conv.sortedDescending().take(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
